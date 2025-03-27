bdd5e208d49ce9da88dacb1ac3a72dfa50ff6fb4
ObaidDev <abdellatif.hour.web@gmail.com>
2025-03-27 14:47:42 +0000

    Update pom.xml

--- src/main/java/com/trackswiftly/utils/App.java
+++ src/main/java/com/trackswiftly/utils/App.java
@@ -1,5 +1,9 @@
 package com.trackswiftly.utils;
 
+
+import com.trackswiftly.utils.annotations.ValidateOwnership;
+import com.trackswiftly.utils.annotations.ValidateOwnerships;
+
 /**
  * Hello world!
  */
@@ -7,4 +11,5 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
 }
