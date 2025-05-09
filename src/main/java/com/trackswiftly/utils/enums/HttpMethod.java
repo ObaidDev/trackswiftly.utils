b5b23bd08ece08ae6c5d038e8d16ab38d8b5157b
ObaidDev <abdellatif.hour.web@gmail.com>
2025-05-09 16:21:43 +0100

    MOve HttpMehtod and Resource to enums

---
+++ src/main/java/com/trackswiftly/utils/enums/HttpMethod.java
@@ -0,0 +1,12 @@
+package com.trackswiftly.utils.enums;
+
+public enum HttpMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    PATCH,
+    OPTIONS,
+    HEAD;
+
+}
---
+++ src/main/java/com/trackswiftly/utils/enums/Resource.java
@@ -0,0 +1,29 @@
+package com.trackswiftly.utils.enums;
+
+public enum Resource {
+    MQTT("mqtt"),
+    DEVICES("gw/devices"),
+    GEOFENCES("gw/geofences"),
+    CALCS("gw/calcs") ,
+    POIS("gw/pois") ;
+
+    private final String path;
+
+    Resource(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    // Validate if a path is a valid resource
+    public static boolean isValidPath(String path) {
+        for (Resource resource : values()) {
+            if (resource.getPath().equals(path)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
