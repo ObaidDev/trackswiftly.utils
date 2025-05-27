5d0663d68bcc3118b32c0f1e258fed2c5cc520eb
ObaidDev <abdellatif.hour.web@gmail.com>
2025-05-27 15:49:40 +0100

    Add Capacity class for managing storage capacity attributes

---
+++ src/main/java/com/trackswiftly/utils/enums/Capacity.java
@@ -0,0 +1,25 @@
+package com.trackswiftly.utils.enums;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Capacity {
+    
+
+    private Double maxWeightKg;
+
+    @Column(name = "max_volume_m3")
+    private Double maxVolumeM3;
+    private Integer maxBoxes;
+    private Integer maxPallets;
+
+}
